7d9aa09b7fe1ef403598243c2125f9bc2ec8b279
Daniel Gonçalves <dfggs@iscte-iul.pt>
2020-06-15 07:00:31 +0100

    delete gitignore

--- Setup - RUN ONLY ONCE!.bat
+++ Setup - RUN ONLY ONCE!.bat
@@ -1,2 +1,2 @@
 Rem Start a cmd window at the current directory
-START cmd.exe /k "ECHO Start configuration... & docker image build -t 'wordpress-with-java:5.4.1' . & docker-compose up & ECHO Configuration Finished! You can now close this window."
+START cmd.exe /k "ECHO Start configuration... & docker image build -t "wordpress-with-java:5.4.1" . & docker-compose up & ECHO Configuration Finished! You can now close this window."
